b5af8975c2d84eaa61810ead7ba82cc4896e2827
Heboxin <heboxin@pku.edu.cn>
2018-05-08 09:08:06 +0800

    x

--- tests_tcp/mpirun.sh
+++ tests_tcp/mpirun.sh
@@ -4,8 +4,8 @@ src_path="/home/cuixiang/hbx/Sendrecv/build"
 ((i=0))
 while read hostip port;do
     echo "start process on ${hostip}"
-    echo "cd ${src_path} && ./test_tcp -i ${i} -f $1 $2 $3 -t $4"
-    ssh -n $hostip "cd ${src_path} && ./test_tcp -i ${i} -f $1 $2 $3 -t $4" &# > out/$i-outf.out 2>&1 &
+    echo "cd ${src_path} && ./test_rdma -i ${i} -f $1 $2 $3 -t $4"
+    ssh -n $hostip "cd ${src_path} && ./test_rdma -i ${i} -f $1 $2 $3 -t $4" &# > out/$i-outf.out 2>&1 &
     ((i++))
 done < start_hostfile
-#./mpirun.sh ~/hbx/Sendrecv/tests_tcp/hostfile -k 1 -t 1000
+#./mpirun.sh ~/hbx/Sendrecv/tests_tcp/hostfile -k 1 1000
